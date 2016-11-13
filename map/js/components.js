b148171482e441477e327de583b581d492276359
askee123 <andrewpat24@gmail.com>
2016-11-12 18:33:17 -0800

    Organized and deleted all appropriately necessary and unnessassary files as well as slightly clearer documentation

---
+++ map/js/components.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+   // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+  $('.modal').modal();
+  $('.fixed-action-btn').openFAB();
+  $('.fixed-action-btn').closeFAB();
+  $('.fixed-action-btn.toolbar').openToolbar();
+  $('.fixed-action-btn.toolbar').closeToolbar();
+ });
--- map/js/maps.js
+++ map/js/maps.js
@@ -1,7 +1,22 @@
-function initMap() {
-    var currentPos = {lat: -34.397, lng: 150.644};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      center: currentPos,
+// 1. MAPS
+//
+// 2. FLOATING ACTION BUTTON
+//
+
+
+// This example adds a search box to a map, using the Google Place Autocomplete
+// feature. People can enter geographical searches. The search box will return a
+// pick list containing a mix of places and predicted search terms.
+
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+// 1. MAPS
+function initAutocomplete() {
+  var currentPos = {lat: -34.397, lng: 150.644};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: currentPos,
       zoom: 12,
       styles: [
            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
@@ -88,58 +103,47 @@ function initMap() {
          zoomControlOptions : {
            position : google.maps.ControlPosition.TOP_RIGHT
          }
+  });
 
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-         //Map.getUiSettings().setMapToolbarEnabled(false);
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  var markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
     });
-    // var iconBase = 'images/';
-    // var icons = {
-    //   happy: {
-    //     icon : iconBase + 'happy.png'
-    //   },
-    //   sad: {
-    //     icon : iconBase + 'sad.jpg'
-    //   },
-    //   joy: {
-    //     icon : iconBase + 'joy.png'
-    //   }
-    // };
-    // function addMarker(feature) {
-    //   var marker = new google.maps.Marker({
-    //     position: feature.position,
-    //     icon : icons[feature.type].icon,
-    //     map: map
-    //   });
-    // }
-    // var features = [
-    //       {
-    //         position: new google.maps.LatLng(-34.405734, 150.598420),
-    //         type: 'happy'
-    //       }, {
-    //         position: new google.maps.LatLng(-34.306742, 150.500734),
-    //         type: 'sad'
-    //       }
-    //     ];
-    //
-    //     for (var i = 0, feature; feature = features[i]; i++) {
-    //      addMarker(feature);
-    //    }
-    //  }
+    markers = [];
 
-   var marker = new google.maps.Marker({
+    // Instantiating different colored markers
+    var marker = new google.maps.Marker({
      icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
      map: map
    });
-    //var infoWindow = new google.maps.InfoWindow({map: map});
 
-    // Try HTML5 geolocation.
+   // Try HTML5 geolocation.
       if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position) {
         var pos = {
           lat: position.coords.latitude,
           lng: position.coords.longitude
         };
-
         //infoWindow.setPosition(pos);
         marker.setPosition(pos);
         map.setCenter(pos);
@@ -150,12 +154,51 @@ function initMap() {
       // Browser doesn't support Geolocation
       handleLocationError(false,marker, map.getCenter());
     }
-  }
 
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
 
-  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
     infoWindow.setContent(browserHasGeolocation ?
                           'Error: The Geolocation service failed.' :
                           'Error: Your browser doesn\'t support geolocation.');
   }
+
+// END MAPS
+
+// 2. FLOATING ACTION BUTTON
+
+
+// END FLOATING ACTION BUTTON
